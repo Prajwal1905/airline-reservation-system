ec301d8075501a7d687036aaa92ac9109d6e3cc8
Prajwal1905 <prajwalkhade19@gmail.com>
2026-05-11 21:50:45 +0530

    Updated server.js - added auth routes

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,17 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const db = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
 
 // Test route
 app.get('/', (req, res) => {
